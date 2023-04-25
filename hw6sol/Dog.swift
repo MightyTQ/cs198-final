ada054f5c0cd89b11795ec8044e71c190aa60f5e
TQ <77231779+MightyTQ@users.noreply.github.com>
2023-04-24 23:35:50 -0700

    Delete hw6sol directory

--- hw6sol/Dog.swift
+++
@@ -1,8 +0,0 @@
-
-import Foundation
-
-/* This is a model or "blueprint" for the JSON data that we will receive from the api! */
-struct Dog: Codable {
-    let message: String
-    let status: String
-}
